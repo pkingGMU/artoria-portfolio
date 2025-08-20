49d7964b4a3774e3e548478fa9d0cad24d71d671
pkingGMU <pking23@gmu.edu>
2025-08-20 15:37:08 -0400

    added images to public

--- src/App.jsx
+++ src/App.jsx
@@ -40,7 +40,7 @@ function App() {
       description:
         "This project was written in MATLAB and was created for classifying physical activity and postural behavior using triaxial accelerometer data collected from Axivity AX3 devices. The pipeline processes raw .cwa files, extracts relevant motion features, and classifies windows of time into activity types (active vs. inactive) and postures (lying, sitting, or standing).",
       link: "https://github.com/pkingGMU/NIH-NIA-Women-s-Health",
-      img1: "/src/assets/Images/ActivityClassification.png",
+      img1: "/assets/Images/ActivityClassification.png",
     },
     AVRO_READER: {
       project_name: ".AVRO Reader for Empatica Embrace Plus",
@@ -52,7 +52,7 @@ function App() {
       project_name: "Open AI Whisper Offline Transcription",
       description:
         "This project was created as a result of my participation in a research study done by GMU's Bioengineering department. The study revolved around interviewing patients with chronic neck pain. The team needed a way to quickly transcribe these hour long interview without having to pay people for a lot of time. A colleague and I using Open AI's Whisper, Segmentation 3.0, and a PII removal model to build an application that could be used totally offline and follow HIPAA compliance. This application is still being used by researches today.",
-      img1: "src/assets/Images/Whisper.jpg",
+      img1: "assets/Images/Whisper.jpg",
       link: "https://github.com/pkingGMU/OpenAi-Whisper-Interview-Transcription",
     },
   };
@@ -63,7 +63,7 @@ function App() {
       description:
         "This is an ongoing passion project for me to learn lua. It is a game that is centered around the core mechanic of rhythm. I can create my own music and export each section as a different midi file. I have a self made matlab parser that can make intricate csv files for each song. Every facet of the game will use the midi information from the different sections. It is still a large work in progress",
       link: "https://github.com/pkingGMU/project-nikki",
-      img1: "src/assets/Images/nikki.png",
+      img1: "assets/Images/nikki.png",
     },
 
     GESTURE_ML: {
@@ -72,13 +72,13 @@ function App() {
       description:
         "This project was created during my intership at the GMU S.M.A.R.T. Lab. I had one semester to create an abstract and present a poster for the lab. As part of a much larger project going on in the lab, I used MATLABs classifcation learner to develop a prototype KNN model.",
       link: "https://github.com/pkingGMU/Gesture_Machine_Learning",
-      img1: "src/assets/Images/knn.png",
+      img1: "assets/Images/knn.png",
     },
     INTERFACE_MOD: {
       project_name: "Stardew Valley Mod: Central Storage Interface",
       description:
         "This is my other ongoing passion project that I'm working on with a friend. It's a mod for Stardew Valley written in C# using the SMAPI framework. It allows a player to have one interface to craft from using the vanilla machines on the players farm. It also has a storage mechanism where a player can obtain hard drives to store items on.",
-      img1: "src/assets/Images/stardew.png",
+      img1: "assets/Images/stardew.png",
     },
   };
 
@@ -107,19 +107,19 @@ function App() {
           />
           <div className="social_box">
             <IconButton
-              img="/src/assets/Icons/github.svg"
+              img="/assets/Icons/github.svg"
               alt="GitHub"
               id="1"
               link="https://github.com/pkingGMU"
             />
             <IconButton
-              img="/src/assets/Icons/icons8-youtube.svg"
+              img="/assets/Icons/icons8-youtube.svg"
               alt="YouTube"
               id="2"
               link="https://www.youtube.com/@artoria-codes/streams"
             />
             <IconButton
-              img="/src/assets/Icons/icons8-linkedin.svg"
+              img="/assets/Icons/icons8-linkedin.svg"
               alt="LinkedIn"
               id="3"
               link="https://www.linkedin.com/in/pkinggmu/"
